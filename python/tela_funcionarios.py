509f0a0ff3bfc2b25e44cd6b74ad5d024a523f57
Victor <victorheremita840@gmail.com>
2025-09-29 19:32:08 -0300

    visual da lista finalizado

--- python/tela_funcionarios.py
+++ python/tela_funcionarios.py
@@ -1,7 +1,7 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QWidget, QFrame, QPushButton, QLabel
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QWidget, QFrame, QPushButton, QLabel, QGraphicsDropShadowEffect
+from PyQt5.QtGui import QIcon, QPixmap, QColor
+from PyQt5.QtCore import QSize, Qt
 from bancoDados import carregarBD
 
 def funcEditar(ui):
@@ -15,99 +15,150 @@ def mostrarFuncionarios(ui):
     cnx = carregarBD()
     cursor = cnx.cursor()
 
-    cursor.execute("SELECT nome, cpf, Email, disponivel FROM Funcionarios")
+    cursor.execute("SELECT nome , disponivel FROM Funcionarios")
     dadosFuncionarios = cursor.fetchall()
 
     tabela = ui.tableWidget
+    colunas = 3
 
     tabela.setRowCount(len(dadosFuncionarios))
-    tabela.setColumnCount(1)
+    tabela.setColumnCount(colunas)
 
-    #region criar o frame
-    frame = QFrame()
-    labelNome = QLabel("teste 1", frame)
-    labelFunc = QLabel("teste 2", frame)
-    botaoExcluir = QPushButton("", frame)
-    botaoEditar = QPushButton("", frame)
 
-    #config dos labels
-    labelNome.setGeometry(10, 10, 80, 20)
-    labelFunc.setGeometry(10, 50, 80, 20)
+    #region configs
+    #espaçamento 
+    esp_horizontal = 30 # espaçamento horizontal
+    esp_vertical = 20 # espaçamento vertical
 
-    labelNome.setStyleSheet('''
-        QLabel{
-        border: none;
-        font-size: 14px;
-        }   
-    ''')
+    for c in range(colunas):
+        tabela.setColumnWidth(c , 220 + esp_horizontal)
 
-    labelFunc.setStyleSheet('''
-        QLabel{
-        border: none;
-        font-size: 14px;
-        }   
-    ''')
+    for r in range(len(dadosFuncionarios)):
+        tabela.setRowHeight(r, 120 + esp_vertical)
+    
+    #header
+    tabela.setHorizontalHeaderLabels([""] * colunas)
+    tabela.setVerticalHeaderLabels([""] * tabela.rowCount())
+    tabela.horizontalHeader().setVisible(False)
+    tabela.verticalHeader().setVisible(False)
 
-    #config dos botões
-    botaoEditar.setGeometry(140, 80, 30, 30)
-    botaoExcluir.setGeometry(180, 80, 30, 30)
-
-    botaoEditar.setStyleSheet('''
-        QPushButton{
-            border: 1px solid rgba(156, 156, 156, 235);
-            background-color: rgb(255, 255, 255);
-            border-radius: 9px;
-        }
-                              
-        QPushButton:hover{
-            background-color:  rgb(234, 236, 240);
-        }
-                               
-        
-        QPushButton:pressed{
-            background-color: rgb(167, 167, 167);
-        }
-    ''')
-
-    botaoExcluir.setStyleSheet('''
-        QPushButton{
-            border: 1px solid rgba(156, 156, 156, 235);
-            background-color: rgb(255, 255, 255);
-            border-radius: 9px;
-        }                       
-        
-        QPushButton:hover{
-            background-color:  rgb(234, 236, 240);
-        }
-                               
-        
-        QPushButton:pressed{
-            background-color: rgb(167, 167, 167);
-        }
-    ''')
-
-    botaoEditar.setIcon(QIcon("Projeto_Oficina_Motomax\imagem\icons\edit.png"))
-    botaoExcluir.setIcon(QIcon("Projeto_Oficina_Motomax\imagem\icons\delete.png"))
-
-    botaoEditar.setIconSize(QSize(20, 20))
-    botaoExcluir.setIconSize(QSize(20, 20))
-
-    #configs do frame
-    frame.setFixedSize(220, 120)
-    frame.setGeometry(0, 0, 220, 120)
-    frame.setStyleSheet('''
-        QFrame{
-        background-color: white;
-        border-radius: 15px;
-        border: 1px solid
-        }
-        ''')
-
-    tabela.setCellWidget(0, 0, frame)
+    tabela.setShowGrid(False)
     #endregion
 
-    #funções botões
-    botaoEditar.clicked.connect()
+    #region criar o frame
+    for i, _funcionario in enumerate(dadosFuncionarios):
+        frame = QFrame()
+
+        textoDisponivel = ""
+        if _funcionario[1] == 0:
+            textoDisponivel = "Indisponível"
+        else:
+            textoDisponivel = "Disponível"
+
+        labelNome = QLabel(f"Nome: {_funcionario[0]}", frame)
+        labelFunc = QLabel(f"Disponibilidade: {textoDisponivel}", frame)
+        botaoExcluir = QPushButton("", frame)
+        botaoEditar = QPushButton("", frame)
+
+        #sombra
+        sombra = QGraphicsDropShadowEffect()
+
+        #region config sombra
+        sombra.setBlurRadius(15)
+        sombra.setOffset(20, 0)
+        sombra.setColor(QColor(60, 60, 60))
+
+        frame.enterEvent(lambda: frame.setGraphicsEffect(sombra))
+        frame.leaveEvent(lambda: frame.setGraphicsEffect(None))
+        #endregion
+
+        #config dos labels
+        labelNome.setGeometry(10 , 10 , 100, 20)
+        labelFunc.setGeometry(10 , 50 , 180, 20)
+
+        labelNome.setStyleSheet('''
+            QLabel{
+            border: none;
+            font-size: 14px;
+            }   
+        ''')
+
+        labelFunc.setStyleSheet('''
+            QLabel{
+            border: none;
+            font-size: 14px;
+            }   
+        ''')
+
+        #config dos botões
+        botaoEditar.setGeometry(140, 80, 30, 30)
+        botaoExcluir.setGeometry(180, 80, 30, 30)
+
+        botaoEditar.setStyleSheet('''
+            QPushButton{
+                border: 1px solid rgba(156, 156, 156, 235);
+                background-color: rgb(255, 255, 255);
+                border-radius: 9px;
+            }
+                                
+            QPushButton:hover{
+                background-color:  rgb(234, 236, 240);
+            }
+                                
+            
+            QPushButton:pressed{
+                background-color: rgb(167, 167, 167);
+            }
+        ''')
+
+        botaoExcluir.setStyleSheet('''
+            QPushButton{
+                border: 1px solid rgba(156, 156, 156, 235);
+                background-color: rgb(255, 255, 255);
+                border-radius: 9px;
+            }                       
+            
+            QPushButton:hover{
+                background-color:  rgb(234, 236, 240);
+            }
+                                
+            
+            QPushButton:pressed{
+                background-color: rgb(167, 167, 167);
+            }
+        ''')
+
+        botaoEditar.setIcon(QIcon("imagem/icons/edit.png"))
+        botaoExcluir.setIcon(QIcon("imagem/icons/delete.png"))
+
+        botaoEditar.setIconSize(QSize(20, 20))
+        botaoExcluir.setIconSize(QSize(20, 20))
+
+        #texto e coisas
+        #for _funcionario in dadosFuncionarios:
+        
+
+        #configs do frame
+        frame.setFixedSize(220, 120)
+        frame.setStyleSheet('''
+            QFrame{
+            background-color: white;
+            border-radius: 15px;
+            border: 1px solid
+            }
+            ''')
+        
+        #funções botões
+        botaoEditar.clicked.connect(lambda: funcEditar())
+
+        #formatação de tabela de widget
+        row = i // colunas
+        col = i % colunas
+
+        tabela.setCellWidget(row, col, frame)
+        #endregion
+
+        
 
 def voltarTelaPrincipal(stackWidget):
     stackWidget.setCurrentIndex(1)
--- python/test.py
+++ python/test.py
@@ -1,43 +1,48 @@
-import timeit
+# import timeit
 
-start = timeit.default_timer()
+# start = timeit.default_timer()
 
-#Your statements here
+# #Your statements here
 
-stop = timeit.default_timer()
+# stop = timeit.default_timer()
 
-print('Time: ', stop - start) 
+# print('Time: ', stop - start) 
 
-myArray = [170, 45, 75, 90, 802, 24, 2, 66]#aqui o valor de maior prioridade é 3 porque é o maior e o menor é 1
-print("Original array:", myArray)
-radixArray = [[], [], [], [], [], [], [], [], [], []]
-maxVal = max(myArray)
-exp = 1
+# myArray = [170, 45, 75, 90, 802, 24, 2, 66]#aqui o valor de maior prioridade é 3 porque é o maior e o menor é 1
+# print("Original array:", myArray)
+# radixArray = [[], [], [], [], [], [], [], [], [], []]
+# maxVal = max(myArray)
+# exp = 1
 
-while maxVal // exp > 0:
-    while len(myArray) > 0:
-        val = myArray.pop()
-        radixIndex = (val // exp) % 10
-        radixArray[radixIndex].append(val)
+# while maxVal // exp > 0:
+#     while len(myArray) > 0:
+#         val = myArray.pop()
+#         radixIndex = (val // exp) % 10
+#         radixArray[radixIndex].append(val)
 
-    for bucket in radixArray:
-        while len(bucket) > 0:
-            val = bucket.pop()
-            myArray.append(val)
+#     for bucket in radixArray:
+#         while len(bucket) > 0:
+#             val = bucket.pop()
+#             myArray.append(val)
 
-    exp *= 10
+#     exp *= 10
 
-print("Sorted array:", myArray)
+# print("Sorted array:", myArray)
 
-import random
-import string
+# import random
+# import string
 
-def gere_codigo_produto() -> str:
-    letters = "".join(random.choice(string.ascii_letters) for _ in range(2))
-    digits = "".join(random.choice(string.digits) for _ in range(4))
-    return f"{letters}-{digits}"
+# def gere_codigo_produto() -> str:
+#     letters = "".join(random.choice(string.ascii_letters) for _ in range(2))
+#     digits = "".join(random.choice(string.digits) for _ in range(4))
+#     return f"{letters}-{digits}"
 
-def gere_codigo_ordem() -> str:
-    letters = "".join(random.choice(string.ascii_letters) for _ in range(3))
-    digits = "".join(random.choice(string.digits) for _ range(3))
-    return f"{digits}-{letters}"
+# def gere_codigo_ordem() -> str:
+#     letters = "".join(random.choice(string.ascii_letters) for _ in range(3))
+#     digits = "".join(random.choice(string.digits) for _ range(3))
+#     return f"{digits}-{letters}"
+
+p = 3
+for i in range(3):
+    print(i // p)
+    print(i % p)
